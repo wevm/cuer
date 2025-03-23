03c7e6a86caf26f73079935bc5b818151902170b
jxom <7336481+jxom@users.noreply.github.com>
2025-03-23 16:33:29 +1100

    wip: jsdoc

--- src/Cuer.tsx
+++ src/Cuer.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import * as QrCode from './QrCode.js'
 
+/**
+ * Renders a QR code with a finder pattern, cells, and an `arena` (if provided).
+ *
+ * @params {@link Cuer.Props}
+ * @returns A {@link React.ReactNode}
+ */
 export function Cuer(props: Cuer.Props) {
   const { arena, ...rest } = props
   return (
@@ -32,10 +38,32 @@ export function Cuer(props: Cuer.Props) {
 export namespace Cuer {
   export type Props = React.PropsWithChildren<
     QrCode.QrCode.Options & {
+      /**
+       * Arena to display in the center of the QR code.
+       *
+       * - `string`: will be rendered as an image.
+       * - `ReactNode`: will be rendered as a node.
+       */
       arena?: React.ReactNode | string | undefined
+      /**
+       * Class name for the root element.
+       */
       className?: string | undefined
+      /**
+       * Foreground color for the QR code.
+       *
+       * @default "currentColor"
+       */
       color?: string | undefined
+      /**
+       * Size for the QR code.
+       *
+       * @default "100%"
+       */
       size?: React.CSSProperties['width'] | undefined
+      /**
+       * Value to encode in the QR code.
+       */
       value: string
     }
   >
@@ -48,9 +76,16 @@ export namespace Cuer {
     qrcode: QrCode.QrCode
   }>(null as never)
 
+  /**
+   * Root component for the QR code.
+   *
+   * @params {@link Root.Props}
+   * @returns A {@link React.ReactNode}
+   */
   export function Root(props: Root.Props) {
     const { children, size = '100%', value, version, ...rest } = props
 
+    // Check if the children contain an `Arena` component.
     const hasArena = React.useMemo(
       () =>
         (
@@ -68,8 +103,10 @@ export namespace Cuer {
       [children],
     )
 
+    // Create the QR code.
     const qrcode = React.useMemo(() => {
       let errorCorrection = props.errorCorrection
+      // If the QR code has an arena, use a higher error correction level.
       if (hasArena && errorCorrection === 'low') errorCorrection = 'medium'
       return QrCode.create(value, {
         errorCorrection,
@@ -112,12 +149,27 @@ export namespace Cuer {
           React.SVGProps<SVGSVGElement>,
           'children' | 'width' | 'height' | 'version'
         > & {
+          /**
+           * Size for the QR code.
+           *
+           * @default "100%"
+           */
           size?: React.CSSProperties['width'] | undefined
+          /**
+           * Value to encode in the QR code.
+           */
           value: string
         }
     >
   }
 
+  /**
+   * Finder component for the QR code. The finder pattern is the squares
+   * on the top left, top right, and bottom left of the QR code.
+   *
+   * @params {@link Finder.Props}
+   * @returns A {@link React.ReactNode}
+   */
   export function Finder(props: Finder.Props) {
     const { className, fill, innerClassName, radius = 1 } = props
     const { cellSize, edgeSize, finderSize } = React.useContext(Context)
@@ -188,6 +240,12 @@ export namespace Cuer {
     }
   }
 
+  /**
+   * Cells for the QR code.
+   *
+   * @params {@link Cells.Props}
+   * @returns A {@link React.ReactNode}
+   */
   export function Cells(props: Cells.Props) {
     const {
       className,
@@ -274,12 +332,35 @@ export namespace Cuer {
       React.SVGProps<SVGPathElement>,
       'className' | 'filter' | 'fill'
     > & {
+      /**
+       * @deprecated @internal
+       */
       hasArena?: boolean | undefined
+      /**
+       * Whether to add an inset to the cells.
+       *
+       * @default true
+       */
       inset?: boolean | undefined
+      /**
+       * Radius scale (between 0 and 1) for the cells.
+       *
+       * - `0`: no radius
+       * - `1`: full radius
+       *
+       * @default 1
+       */
       radius?: number | undefined
     }
   }
 
+  /**
+   * Arena component for the QR code. The arena is the area in the center
+   * of the QR code that is not part of the finder pattern.
+   *
+   * @params {@link Arena.Props}
+   * @returns A {@link React.ReactNode}
+   */
   export function Arena(props: Arena.Props) {
     const { children } = props
     const { arenaSize, cellSize, edgeSize } = React.useContext(Context)
